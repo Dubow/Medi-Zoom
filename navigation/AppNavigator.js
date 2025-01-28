0d638723163f91b3f321bcd43c7602d7dcdbfff0
Abdirahman <abdirahmandubow4@gmail.com>
2025-01-28 17:41:49 +0800

    adding navigation bar to the app

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "../screens/LoginScreen";
+import SignUpScreen from "../screens/SignupScreen";
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
