6fa0706e00aedc65d6ee2080377516d26313ad9d
Abdirahman <abdirahmandubow4@gmail.com>
2025-01-28 18:46:18 +0800

    updating server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,19 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const { sequelize } = require("./models");
 require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { sequelize } = require("./models/index");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running!");
-});
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Database connected...");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.error("Database connection error:", err));
+sequelize.sync().then(() => {
+  console.log("Database connected & models synced!");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
